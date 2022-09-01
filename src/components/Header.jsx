8beffd0923ee188323030dc323d011ea2be02cff
kayckhirt <kayckhirt15@gmail.com>
2022-08-31 22:11:57 -0300

    requisito 3, quem ta triste, não ta mais

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,44 @@ import { getUser } from '../services/userAPI';
 class Header extends React.Component {
   state = {
     loading: false,
+    name: '',
   };
 
+  componentDidMount() {
+    this.recoverName();
+    this.setState({
+      loading: true,
+    });
+  }
+
   recoverName = async () => {
-    await getUser;
+    this.setState({
+      loading: true,
+    });
+    const name = await getUser();
+    this.setState({
+      name,
+    });
+    this.setState({
+      loading: false,
+    });
   };
 
   render() {
+    const { name, loading } = this.state;
     return (
-      <header data-testid="header-component" />
+      <header data-testid="header-component">
+        <div data-testid="header-user-name">
+          Eai Dj,
+          {
+            ' '
+          }
+          { name.name }
+          {
+            loading && <Loading />
+          }
+        </div>
+      </header>
     );
   }
 }
